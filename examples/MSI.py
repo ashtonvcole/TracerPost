c12b43fcc112e341b26f731a70eb538ccc30f356
ashtonvcole <asthoncole1028@gmail.com>
2026-09-01 19:29:26 -0500

    Add skeletons

---
+++ examples/MSI.py
@@ -0,0 +1,15 @@
+def main():
+    # Todo
+    # Define parameters like time step etc
+    # Build mesh from fort.14
+    # Elements/function spaces
+    # Trial/test functions
+    # IC
+    # BC
+    # Weak form
+    # Assembly
+    # Solution loop/writing
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ src/equations.py
@@ -0,0 +1,11 @@
+class HyperbolicConservationLaw:
+    pass
+
+class ParabolicConservationLaw:
+    pass
+
+class SWEAdvection(HyperbolicConservationLaw):
+    pass
+
+class SWEAdvectionDiffusion(ParabolicConservationLaw):
+    pass
